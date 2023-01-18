54dd8d59dd8b2dfe5bbfa72dce9a631cd4381ac3
DirikTi <ertugruldirik35@gmail.com>
2023-01-18 22:40:20 +0300

    Create table posts

--- CREATE_TABLES/SQL.sql
+++ CREATE_TABLES/SQL.sql
@@ -1,27 +1,45 @@
-DROP DATABASE IF EXISTS youtube;
+DROP DATABASE IF EXISTS fake_socialmedias;
 
-CREATE DATABASE youtube;
+CREATE DATABASE fake_socialmedias;
 
-USE youtube;
+USE fake_socialmedias;
+
+-- VARIABLE
 
 CREATE TABLE IF NOT EXISTS users (
 	userId INT UNSIGNED NOT NULL AUTO_INCREMENT,
     username CHAR(127) NOT NULL,
-    email CHAR(127) NOT NULL UNIQUE,
+    email CHAR(127) NOT NULL,
     phone CHAR(15) NOT NULL,
     name CHAR(63) NOT NULL,
-    userToken BINARY(16),
-    userTokenText VARCHAR(36) GENERATED ALWAYS AS
-		 (INSERT(
+    avatar CHAR(150) NOT NULL,
+    avatarUrl TEXT GENERATED ALWAYS AS (CONCAT()) VIRTUAL
+    isSpecialUser TINYINT(1) NOT NULl,
+    loginToken BINARY(16),
+    loginTokenText VARCHAR(36) GENERATED ALWAYS AS
+		(INSERT(
 			INSERT(
-			  INSERT(
-				INSERT(HEX(userToken),9,0,'-'),
-				14,0,'-'),
-			  19,0,'-'),
+                INSERT(
+                    INSERT(HEX(loginToken),9,0,'-'),
+                    14,0,'-'),
+                19,0,'-'),
 			24,0,'-')
-		 ) virtual,
+		) VIRTUAL,
+    lastLogin_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP();
     updated_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
     created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
-    PRIMARY KEY (userId)
+    PRIMARY KEY (userId),
+    UNIQUE (email, username)
 ) ENGINE=InnoDB;
 
+CREATE TABLE IF NOT EXISTS posts ( 
+    postId BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    typeId INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    userId INT UNSIGNED NOT NULL,
+    typeText TEXT(512),
+    postUrls TEXT NOT NULL,
+    type ENUM ('photo', 'video', 'text', 'mix')
+    isDelete TINYINT(1) DEFAULT 0,
+    updated_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
+    created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
+) ENGINE=InnoDB;
