b9b6e9f1cf328b843814239bea1b2fc18aa32faa
DirikTi <ertugruldirik35@gmail.com>
2023-01-19 23:39:51 +0300

    notification table and v_users view

---
+++ VIEWS/SQL.sql
@@ -0,0 +1,20 @@
+CREATE VIEW v_users
+AS
+SELECT u.userId, u.username, u.email, u.phone, u.name, u.avatar, u.avatarUrl, u.isSpecialUser, u.phoneToken,
+u.lastLogin_date, u.updated_date, u.created_date, (
+    SELECT COUNT()
+    FROM releationships r
+    WHERE r.userOneId=u.userId OR r.userTwoId=u.userId
+) AS friends_count,
+COALESCE((
+    SELECT JSON_ARRAYAGG(
+        JSON_OBJECT(
+            'id', n.notificationId,
+            'title', n.title,
+            'body', n.body,
+            'created_date', n.created_date
+        )
+    ) FROM notifications n
+    WHERE n.userId=u.userId AND isShowed=0
+), '[]') AS notifications
+FROM users u
