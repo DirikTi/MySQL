d769f33957c9a39703795d32da97db623ae10ba6
DirikTi <ertugruldirik35@gmail.com>
2023-01-30 21:05:23 +0300

    fixed type Insert data

--- SQL_FILES/INSERT_DATA/SQL.sql
+++ SQL_FILES/INSERT_DATA/SQL.sql
@@ -3,6 +3,9 @@ use fake_socialmedias;
 DROP TABLE IF EXISTS temp_type_datas;
 CREATE TABLE temp_type_datas (id TINYINT(2), name VARCHAR(150), relation VARCHAR(150));
 
+DROP TEMPORARY TABLE IF EXISTS row_numb_datas;
+CREATE TEMPORARY TABLE row_numb_datas (row_numb TINYINT(4), name CHAR(127), relation CHAR(127));
+
 INSERT INTO temp_type_datas (id, name, relation) VALUES
 (1, 'main', NULL),
 
@@ -28,6 +31,10 @@ INSERT INTO temp_type_datas (id, name, relation) VALUES
 (17, 'tennis', 'sports');
 
 
+INSERT INTO row_numb_datas (row_numb, name, relation)
+SELECT ROW_NUMBER() OVER (PARTITION BY relation) AS row_num, name, relation
+FROM temp_type_datas;
+
 WITH RECURSIVE cte AS 
 (
     SELECT id, name AS typeName, CAST(CONCAT('/', id) AS CHAR(20)) AS hieararcyid
@@ -35,12 +42,15 @@ WITH RECURSIVE cte AS
     WHERE relation IS NULL
     UNION ALL
     SELECT ttd.id, ttd.name AS  typeName, CONCAT(c.hieararcyid, '/', (
-        SELECT ROW_NUMBER() OVER (PARTITION BY ttd2.relation) AS row_num
-        FROM temp_type_datas ttd2
-        WHERE ttd2.id=c.id
+        SELECT row_numb
+        FROM row_numb_datas ttd2
+        WHERE ttd2.name=ttd.name
         LIMIT 1
     )) AS hieararcyid
     FROM temp_type_datas ttd INNER JOIN cte c ON c.typeName=ttd.relation
     LIMIT 15
 )
 SELECT * FROM cte;
+
+DROP TEMPORARY TABLE IF EXISTS row_numb_datas;
+DROP TABLE IF EXISTS temp_type_datas;
